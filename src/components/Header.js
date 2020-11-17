8930cbee6cc082cd2cbc28655c39393faebe0f5d
nomanferoz <nomanferoz007@gmail.com>
2020-11-17 20:09:34 +0500

    git

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
+import { Link } from "react-router-dom";
+function Header() {
+  return (
+    <div className="header">
+      <Link to="/">
+        <img
+          className="header__icon"
+          src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+          alt=""
+        />
+      </Link>
+
+      <div className="header__center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+      <div className="header__right">
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+
+        <Avatar src=" https://st2.depositphotos.com/4060975/8907/v/450/depositphotos_89072330-stock-illustration-rat-flat-icon-illustration.jpg " />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
